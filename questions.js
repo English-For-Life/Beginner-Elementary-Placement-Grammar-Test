9a75768abdb38b84d546895608b09b646b1e5329
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-06-10 10:34:38 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,206 @@
+//Array of objects
+const quiz = [
+    {
+        q:"____'s your name? - Thomas.",
+        options:["What", "How", "Who", "Where"],
+        answer: 0
+    },
+    {
+        q:"This is Lucy and her brother Dan. ____ my friends.",
+        options:["We're", "They're", "You're", "I'm"],
+        answer: 1
+    },
+    {
+        q:"____ - I'm from Italy.",
+        options:["Where from you are?", "Where you are from?", "Where are you from?", "From where you are?"],
+        answer: 2
+    },
+    {
+        q:"I'm from Milan. ____ is in Italy.",
+        options:["She", "He", "They", "It" ],
+        answer: 3
+    },
+    {
+        q:"Excuse me, how ____ your last name? - R-I-L-E-Y.",
+        options:["do you spell", "spell", "you spell", "spell you"],
+        answer: 0
+    },
+    {
+        q:"Oh, ____ are my keys!",
+        options:["it", "these", "this", "that"],
+        answer: 1
+    },
+    {
+        q:"I'd like ____ omelette, please.",
+        options:["a", "-", "an", "two"],
+        answer: 2   
+    },
+    {
+        q:"And here is your ____ .",
+        options:["an desk", "desks", "a desk", "desk"],
+        answer: 3
+    },
+    {
+        q:"My name's Pete and this is Sylvia. ____ doctors from France.",
+        options:["We're", "They're", "She's", "I'm"],
+        answer: 0
+    },
+    {
+        q:"Sorry, ____ Paul. My name's Eric.",
+        options:["I isn't", "I'm not", "I is not", "I aren't"],
+        answer: 1
+    },
+    {
+        q:"____ - No, he isn't.",
+        options:["Are they teachers?", "Are you from Italy?", "Is Mr Banning a teacher?", "Is this your phone?"],
+        answer: 2
+    },
+    {
+        q:"____ is the school? It's 50 years old.",
+        options:["How many years", "How much years", "What years", "How old"],
+        answer: 3
+    },
+    {
+        q:"What is ____ ?",
+        options:["Mary's job", "job's Mary", "Mary job", "job Mary"],
+        answer: 0
+    },
+    {
+        q:"Your bag is next ____ the table.",
+        options:["of", "to", "on", "in"],
+        answer: 1
+    },
+    {
+        q:"____ are the keys? - On the table.",
+        options:["When", "Who", "Where", "What"],
+        answer: 2
+    },
+    {
+        q:"I go to work ____ train.",
+        options:["in", "for", "with", "by"],
+        answer: 3
+    },
+    {
+        q:"She ____ a dog.",
+        options:["doesn't have", "not have", "don't have", "don't has"],
+        answer: 0
+    },
+    {
+        q:"Stephen ____ in our company.",
+        options:["work", "works", "is work", "working"],
+        answer: 1
+    },
+    {
+        q:"____ they live in London?",
+        options:["Are", "Is", "Do", "Does"],
+        answer: 2
+    },
+    {
+        q:"____ to the cinema.",
+        options:["We not often go", "We don't go often", "Often we don't go", "We don't often go"],
+        answer: 3
+    },
+    {
+        q:"When do you play tennis? - ____ Mondays.",
+        options:["On", "It", "At", "By"],
+        answer: 0
+    },
+    {
+        q:"What time ____ work?",
+        options:["does he starts", "does he start", "do he starts", "starts he"],
+        answer: 1
+    },
+    {
+        q:"____ two airports in the city.",
+        options:["This is", "It is", "There are", "There is"],
+        answer: 2
+    },
+    {
+        q:"There aren't ____ here.",
+        options:["a restaurant", "any restaurant", "a restaurants", "any restaurants" ],
+        answer: 3
+    },
+    {
+        q:"I'm afraid it's ____ .",
+        options:["an expensive hotel", "expensive a hotel", "expensive hotel", "expensive hotel"],
+        answer: 0
+    },
+    {
+        q:"They ____ popular TV programmes in the 1980s.",
+        options:["was", "were", "is", "are"],
+        answer: 1
+    },
+    {
+        q:"____ at school last week?",
+        options:["You were", "Do you were", "Were you", "Was you"],
+        answer: 2   
+    },
+    {
+        q:"Brad Pitt is a popular actor but I don't like ____ .",
+        options:["them", "her", "his", "him"],
+        answer: 3
+    },
+    {
+        q:"We ____ the film last week.",
+        options:["saw", "were see", "sees", "see"],
+        answer: 0
+    },
+    {
+        q:"He ____ tennis with me yesterday.",
+        options:["doesn't played", "didn't play", "not played", "didn't played"],
+        answer: 1
+    },
+    {
+        q:"She was born ____ May 6th, 1979.",
+        options:["from", "at", "on", "in"],
+        answer: 2
+    },
+    {
+        q:"Where ____ last summer?",
+        options:["do you went", "you went", "did you went", "did you go"],
+        answer: 3
+    },
+    {
+        q:"Were you at the shops at 5 p.m. yesterday? - No, I ____ .",
+        options:["weren't", "wasn't", "didn't", "am not"],
+        answer: 0
+    },
+    {
+        q:"Excuse me, ____ is the T-shirt? It's ₤25.99.",
+        options:["what expensive", "how much", "how price", "how many"],
+        answer: 1
+    },
+    {
+        q:"She's only four but she ____ .",
+        options:["cans reads", "cans read", "can read", "can reads"],
+        answer: 2
+    },
+    {
+        q:"This party is boring. We ____ a good time.",
+        options:["don't having", "aren't have", "don't have", "aren't having"],
+        answer: 3
+    },
+    {
+        q:"Sorry, I ____ you at the moment.",
+        options:["can't help", "can't helping", "can't helps", "don't can help"],
+        answer: 0
+    },
+    {
+        q:"I ____ my computer very often.",
+        options:["am not use", "don't use", "doesn't use", "am not using"],
+        answer: 1
+    },
+    {
+        q:"It's my mum's birthday next week. I ____ her a present.",
+        options:["buy", "buys", "am going to buy", "buying"],
+        answer: 2
+    },
+    {
+        q:"What ____ do after school today?",
+        options:["do you", "you", "are you", "are you going to"],
+        answer: 3
+    }
+    
+
+
+]
